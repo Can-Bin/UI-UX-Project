eff5d71da83b34a547028595275fd25a2cde27c9
Can Bin <canbin60@gmail.com>
2023-02-20 17:26:35 +0300

    new features added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import "./App.css"
-
 import {Article, Brand, CTA, Feature, Navbar} from "./components";
-import {Footer, Blog, Possibility, Features, WhatGOT3, Header} from "./containers"
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from "./containers"
 
 const App = () => {
     
@@ -10,7 +9,15 @@ const App = () => {
         <div className='App'> 
             <div className='gradient__bg'>
                 <Navbar/>
+                <Header/>
             </div>
+            <Brand/>
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
         </div>
     )
 }

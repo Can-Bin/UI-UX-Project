67d07fcfa074823c1f56ab3b1112da2e8de82143
Can Bin <canbin60@gmail.com>
2023-02-24 17:11:40 +0300

    whatgpt3 added

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,43 @@ import "./whatGPT3.css"
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__padding'>
+      <div className='gpt3__whatgpt3-top'>
+        <div className='gpt3__whatgpt3-top__left'>
+          <div></div>
+          <p>What is GPT-3</p>
+        </div>
+        <div className='gpt3__whatgpt3-top__right'>
+          <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+        </div>
+      </div>
+      <div className='gpt3__whatgpt3-mid'>
+        <div className='gpt3__whatgpt3-mid__left'>
+          <p>The possibilities are beyond your imagination</p>
+        </div>
+        <div className='gpt3__whatgpt3-mid__right'>
+          <p>Explore The Library</p>
+        </div>
+      </div>
+      <div className='gpt3__whatgpt3-bottom'>
+        <div className='gpt3__whatgpt3-bottom__element'>
+          <div></div>
+          <h3>Chatbots</h3>
+          <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
+        </div>
+        <div className='gpt3__whatgpt3-bottom__element'>
+          <div></div>
+          <h3>Knowledgebase</h3>
+          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
+        </div>
+        <div className='gpt3__whatgpt3-bottom__element'>
+          <div></div>
+          <h3>Education</h3>
+          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 

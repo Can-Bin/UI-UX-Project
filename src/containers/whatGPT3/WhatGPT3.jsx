24143157f57a030db5578caa53904428bde0a68f
Can Bin <canbin60@gmail.com>
2023-02-27 19:04:50 +0300

    components updated

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,44 +1,22 @@
 import React from 'react'
 import "./whatGPT3.css"
+import Feature from '../../components/feature/Feature'
 
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section__padding'>
-      <div className='gpt3__whatgpt3-top'>
-        <div className='gpt3__whatgpt3-top__left'>
-          <div></div>
-          <p>What is GPT-3</p>
-        </div>
-        <div className='gpt3__whatgpt3-top__right'>
-          <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
-        </div>
+    <div className='gpt3__whatgpt3 section__margin'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
       </div>
-      <div className='gpt3__whatgpt3-mid'>
-        <div className='gpt3__whatgpt3-mid__left'>
-          <p>The possibilities are beyond your imagination</p>
-        </div>
-        <div className='gpt3__whatgpt3-mid__right'>
+      <div className='gpt3__whatgpt3-heading'>   
+          <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
           <p>Explore The Library</p>
-        </div>
       </div>
-      <div className='gpt3__whatgpt3-bottom'>
-        <div className='gpt3__whatgpt3-bottom__element'>
-          <div></div>
-          <h3>Chatbots</h3>
-          <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
-        </div>
-        <div className='gpt3__whatgpt3-bottom__element'>
-          <div></div>
-          <h3>Knowledgebase</h3>
-          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
-        </div>
-        <div className='gpt3__whatgpt3-bottom__element'>
-          <div></div>
-          <h3>Education</h3>
-          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
-        </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." title="Chatbots"/>
+        <Feature text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" title="Knowledgebase"/>
+        <Feature text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" title="Education"/>
       </div>
-
     </div>
   )
 }

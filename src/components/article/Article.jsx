07205e88b0ad46afcab335f2af8407d261551cfc
Can Bin <canbin60@gmail.com>
2023-03-07 17:46:48 +0300

    Article component was created

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./article.css";
 
-const Article = () => {
+const Article = ({image, date, title, link}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__article'>
+      <div className='gpt3__article-pic'>
+        <img src={image} alt="image" />
+      </div>
+      <div className='gpt3__article-content'>
+        <div className='gpt3__article-content__top' >
+          <p>{date}</p>
+          <p>{title}</p>
+        </div>
+        <div className='gpt3__article-content__bottom' >
+          <p><a href="#">{link}</a></p>
+        </div>
+      </div>
+    </div>
   )
 }
 

f3cfc8a29d659c54d380698878f9d569ea127eb0
Can Bin <canbin60@gmail.com>
2023-03-07 13:50:20 +0300

    new component was added

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gpt3__cta section__margin'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <p>Register today & start exploring the endless possiblities.</p>
+      </div>
+      <div className='gpt3__cta-started'>
+        <button>Get Started</button>
+      </div>
+    </div>
   )
 }
 

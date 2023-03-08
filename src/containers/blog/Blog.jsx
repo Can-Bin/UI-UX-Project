7a85618be12a520302406996fa272cf59363cbf3
Can Bin <canbin60@gmail.com>
2023-03-08 17:24:17 +0300

    blog added

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./blog.css"
+import Article from "../../components/article/Article"
+import blog1 from "../../assets/blog01.png"
+import blog2 from "../../assets/blog02.png"
+import blog3 from "../../assets/blog03.png"
+import blog4 from "../../assets/blog04.png"
+import blog5 from "../../assets/blog05.png"
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='section__margin'>
+      <div className='gpt3__blog-content'>
+        <h1>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-article'>
+        <div className='gpt3__blog-article__b1'><Article image={blog1} date="Agu 28,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" link="Read Full Article"/></div>
+        <div><Article image={blog2} date="Agu 28,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" link="Read Full Article"/></div>
+        <div><Article image={blog3} date="Agu 28,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" link="Read Full Article"/></div>
+        <div><Article image={blog4} date="Agu 28,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" link="Read Full Article"/></div>
+        <div><Article image={blog5} date="Agu 28,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" link="Read Full Article"/></div>
+      
+      </div>
+    </div>
   )
 }
 

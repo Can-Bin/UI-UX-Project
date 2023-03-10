1d7d128116fd8e995be6e7e473f463cd51e8f88e
Can Bin <canbin60@gmail.com>
2023-03-10 15:17:21 +0300

    footer added

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,57 @@
-import "./header.css"
+import "./footer.css"
 const Footer = () => {
     return (
-      <div>Footer</div>
+      <div className="gpt3__footer" id="blog">
+        <div className="gpt3__footer-heading">
+          <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+          <button >Request Early Access</button>
+        </div>
+        <div className="gpt3__footer-content">
+          <div className="gpt3__footer-content__links">
+            <div className="gpt3__footer-content__links-left">
+              <h1>GPT-3</h1>
+              <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+            </div>
+            <div className="gpt3__footer-content__links-right">
+              <div className="gpt3__footer-content__links-right__group">
+                <div>
+                  <p>Links</p>
+                </div>
+                <div>
+                  <p>Overons</p>
+                  <p>Social Media</p>
+                  <p>Counters</p>
+                  <p>Contact</p>
+                </div>
+              </div>
+              <div className="gpt3__footer-content__links-right__group">
+                <div>
+                  <p>Company</p>
+                </div>
+                <div>
+                  <p>Terms & Conditions</p>
+                  <p>Privacy Policy</p>
+                  <p>Contact</p>
+                </div>
+              </div>
+              <div className="gpt3__footer-content__links-right__group">
+                <div>
+                  <p>Get in Touch</p>
+                </div>
+                <div>
+                  <p>Crechterwoord K12 182 DK Alknjkcb</p>
+                  <p>085-132567</p>
+                  <p>info@payme.net</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="gpt3__footer-content__copyright">
+            <p>© 2021 GPT-3. All rights reserved.</p>
+          </div>
+        </div>
+
+      </div>
     )
   }
 
